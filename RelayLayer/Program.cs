d04faeac303804164da047770c749413c0a56fb9
Lalli-Oni <lalli.oni@gmail.com>
2015-11-24 17:47:50 +0100

    Faking room sensor data now but has some issues, likely related with the multi-threaded access of the temperature & light field. Fake oven data is working sufficiently.

--- RelayLayer/Program.cs
+++ RelayLayer/Program.cs
@@ -18,6 +18,7 @@ namespace RelayLayer
         /// <param name="args">Command line arguments</param>
         static void Main(string[] args)
         {
+            Console.WriteLine("Controls: 'F' for fake data, 'X' to quit");
             string cmdInput = "";
             while (cmdInput.ToLower() != "x")
             {
@@ -39,44 +40,30 @@ namespace RelayLayer
             Input inp = new Input();
             while (true)
             {
+                List<DataModel[]> dataSet = new List<DataModel[]>();
                 DateTime startSecond = new DateTime(1990, 1, 1);
-                List<DataModel> dataSet = new List<DataModel>();
                 bool newSecond = false;
-                while (newSecond == false)
+                while (!newSecond)
                 {
-                    DataModel data = inp.StartFakingData();
+                    //Gets fake data
+                    DataModel[] sensorData = inp.FakeData();
 
+                    //Checks to see if any data has been collected in this second
                     if (!dataSet.Any())
                     {
-                        startSecond = data.TimeOfData;
+                        //Sets the start of the second
+                        startSecond = sensorData[0].TimeOfData;
                     }
 
                     //Checks if the dataset has enough data to average out and if one second has passed
-                    if (dataSet.Count > 2 && DateTime.Compare(startSecond.AddSeconds(1), data.TimeOfData) < 0)
-                    //Does averages on temperature and light, sends data and makes sure the loop doesn't restart
+                    if (dataSet.Count > 1 && DateTime.Compare(startSecond.AddSeconds(1), sensorData[0].TimeOfData) < 0)
                     {
-                        int tempSum = 0;
-                        int lightSum = 0;
-                        int nrOfData = dataSet.Count;
-
-                        foreach (DataModel datas in dataSet)
-                        {
-                            tempSum += datas.Temperature;
-                            lightSum += datas.Light;
-                        }
-
-                        DataModel second = new DataModel()
-                        {
-                            SensorName = data.SensorName,
-                            TimeOfData = startSecond,
-                            Temperature = tempSum/nrOfData,
-                            Light = lightSum/nrOfData
-                        };
-
-                        sendToWebService(second);
+                        //Averages the whole data set for oven and room data seperately
+                        DataModel[] dataToSend = AverageDataSet(dataSet);
+                        SendToWebService(dataToSend);
                         newSecond = true;
                     }
-                    dataSet.Add(data);
+                    dataSet.Add(sensorData);
                 }
             }
         }
@@ -86,9 +73,52 @@ namespace RelayLayer
         /// The web service then updates the information in the database.
         /// </summary>
         /// <param name="secData">Holds the average data for each second, the time when it's gotten and the name of sensor</param>
-        private static void sendToWebService(DataModel secData)
+        private static void SendToWebService(DataModel[] secData)
         {
-            Console.WriteLine("Data sent: " + secData.ToString());
+            Console.WriteLine("Data sent: " + secData[0].ToString());
+            //Console.WriteLine("Data sent: " + secData[1].ToString());
+        }
+
+        public static DataModel[] AverageDataSet(List<DataModel[]> dataSet)
+        {
+            int tempOvenSum = 0;
+
+            int tempRoomSum = 0;
+            int lightRoomSum = 0;
+
+            int nrOfData = dataSet.Count;
+
+            foreach (DataModel[] datas in dataSet)
+            {
+                tempOvenSum += datas[0].Temperature;
+
+                tempRoomSum += datas[1].Temperature;
+                lightRoomSum += datas[1].Light;
+            }
+
+            int avgOvenTemp = tempOvenSum / nrOfData;
+
+            int avgRoomTemp = tempRoomSum / nrOfData;
+            int avgRoomLight = lightRoomSum / nrOfData;
+
+            DataModel averagedOvenData = new DataModel()
+            {
+                Light = 0,
+                Temperature = avgOvenTemp,
+                SensorName = dataSet[0][0].SensorName,
+                TimeOfData = dataSet[0][0].TimeOfData
+            };
+            DataModel averagedRoomData = new DataModel()
+            {
+                Light = avgRoomTemp,
+                Temperature = avgRoomLight,
+                SensorName = dataSet[0][1].SensorName,
+                TimeOfData = dataSet[0][1].TimeOfData
+            };
+
+            DataModel[] averagedData = new DataModel[2] {averagedOvenData, averagedRoomData};
+
+            return averagedData;
         }
     }
 }
