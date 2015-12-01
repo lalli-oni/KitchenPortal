98ec5edcd924f249e81e0763d5dc35ca57b6c197
Lalli-Oni <lalli.oni@gmail.com>
2015-12-01 12:51:11 +0100

    CheckOvenTemp() now keeps on checking with the db until the temperature is reached or throws an exception.

--- WCFServiceWebRole1/DBAImplementations/SQLImplementation.cs
+++ WCFServiceWebRole1/DBAImplementations/SQLImplementation.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using WCFServiceWebRole1.Model;
 
@@ -42,31 +45,47 @@ namespace WCFServiceWebRole1.DBAImplementations
 
         public bool CheckOvenTemp(int temperature)
         {
-            TimeSpan dateSpan = new TimeSpan(1,0,0,0);
             DateTime date = DateTime.Now;
+            //Creates a timespan to find yesterday and tomorrow
+            TimeSpan dateSpan = new TimeSpan(1, 0, 0, 0);
             DateTime yesterday = new DateTime(date.Year, date.Month, date.Day, 23, 59, 59);
             yesterday =  yesterday.Subtract(dateSpan);
             string yesterdaystring = yesterday.ToString("yyyy-MM-dd HH:mm:ss");
             DateTime tomorrow = new DateTime(date.Year, date.Month, date.Day, 00, 00, 00);
             tomorrow = tomorrow.AddDays(1);
             string tomorrowstring =  tomorrow.ToString("yyyy-MM-dd HH:mm:ss");
+            //The SQL command sent to the database manager
             string command = "SELECT TOP 1 temperature from SensorData WHERE sensorName = 'OVEN' AND  timeOfData >= '" + yesterdaystring + "' AND timeOfData < '" + tomorrowstring +  "' ORDER BY timeOfData";
 
 
-            using (SqlConnection connection =new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 SqlCommand sqlCommand = new SqlCommand(command,connection);
+                //TODO: Error handling (timeout, cooling?...)
 
                 connection.Open();
-                int sqlResult = (int) sqlCommand.ExecuteScalar();
-
-                if (sqlResult >= temperature )
+                //While the connection is open it checks if the temperature is reached
+                //When it is reached it closes the connection and returns true
+                while (connection.State == ConnectionState.Open)
                 {
-                    return true;
-                }             
-               
+                    try
+                    {
+                        int sqlResult = (int)sqlCommand.ExecuteScalar();
+
+                        if (sqlResult >= temperature)
+                        {
+                            connection.Close();
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        connection.Close();
+                        return false;
+                    }
+                    Thread.Sleep(100);
+                }
+                return true;
             }
-            return true;
         }
     }
 }
