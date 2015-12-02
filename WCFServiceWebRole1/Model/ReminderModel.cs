a9bcba5b9920bfefebbc0ee103ddada842ef08aa
Lalli-Oni <lalli.oni@gmail.com>
2015-12-02 13:39:19 +0100

    Added a check in SetReminderAsync() that checks if there are any reminders active and returns false if there are.

---
+++ WCFServiceWebRole1/Model/ReminderModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFServiceWebRole1.Model
+{
+    public class ReminderModel
+    {
+        private int _desiredTemperature;
+        private DateTime _timeOfStart;
+        private string _userId;
+
+        public int DesiredTemperature
+        {
+            get { return _desiredTemperature; }
+            set { _desiredTemperature = value; }
+        }
+
+        public DateTime TimeOfStart
+        {
+            get { return _timeOfStart; }
+            set { _timeOfStart = value; }
+        }
+
+        //Multiple users not yet implemented
+        public string UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+    }
+}
